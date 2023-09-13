7edaf27ce4c36610805498edd85201d7802593e5
Sandeep Kumar <siddhivinayak.sk@gmail.com>
2023-09-13 09:30:08 +0530

    Create wiremock-start.bat

---
+++ wiremock-start.bat
@@ -0,0 +1,18 @@
+:: --disable-http
+:: --https-port
+:: --bind-address
+:: --https-keystore --keystore-type --keystore-password --key-manager-password 
+:: --https-truststore --truststore-type --truststore-password
+:: --https-require-client-cert
+:: --verbose
+:: --record-mappings
+:: --match-headers
+:: --proxy-all --preserve-host-header --proxy-via 
+:: --enable-browser-proxying --ca-keystore --ca-keystore-password --ca-keystore-type
+:: --trust-all-proxy-targets --trust-proxy-target 
+:: --no-request-journal
+
+
+
+
+java -jar C:\sandeep\sandbox\misc\wiremock\wiremock-jre8-standalone-2.35.0.jar --port 81 --root-dir C:\sandeep\sandbox\misc\wiremock
